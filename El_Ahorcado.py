02f772f17be7b0328c7a17d1751ebe36ec601f0c
vazquezj063 <vazquezj063@gmail.com>
2021-04-04 11:54:44 -0300

    otro test de prueba

---
+++ El_Ahorcado.py
@@ -0,0 +1,161 @@
+import random
+HANGMAN_PICS = ['''
+  +---+
+      |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |
+      |
+      |
+     ===''', '''
+  +---+
+  O   |
+  |   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|   |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+      |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ /    |
+     ===''', '''
+  +---+
+  O   |
+ /|\  |
+ / \  |
+     ===''', '''
+  +---+
+ [O   |
+ /|\  |
+ / \  |
+     ===''', '''
+  +---+
+ [O]  |
+ /|\  |
+ / \  |
+     ===''']
+words = {'Colors':'red orange yellow green blue indigo violet white black brown'.split(),
+'Shapes':'square triangle rectangle circle ellipse rhombus trapazoid chevron pentagon hexagon septagon octogon'.split(),
+'Fruits':'apple orange lemon lime pear watermelon grape grapefruit cherry banana cantalope mango strawberry tomato'.split(),
+'Animals':'bat bear beaver cat cougar crab deer dog donkey duck eagle fish frog goat leech lion lizard monkey moose mouse otter owl panda python rabbit rat shark sheep skunk squid tiger turkey turtle weasel whale wolf wombat zebra'.split()}
+
+def getRandomWord(wordDict):
+    ''' This function returns a random string from the passed dictionary of lists of strings, and the key also.'''
+    
+    # Primero, seleccione al azar una clave del diccionario:
+    wordKey = random.choice(list(wordDict.keys()))
+
+    # En segundo lugar, seleccione al azar una palabra de la lista de claves en el diccionario:
+    wordIndex = random.randint(0, len(wordDict[wordKey]) - 1)
+    #---->>AQUI PUSE LA PISTA<<---- 
+    print(f"Pista: contar {wordIndex} palabras, recuerde que debe comenzar contando desde 0")
+    return [wordDict[wordKey][wordIndex], wordKey]
+
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_PICS[len(missedLetters)])
+    print()
+
+    print('Missed letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print()
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)): # reemplace los espacios en blanco con letras correctamente adivinadas
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks: # mostrar la palabra secreta con espacios entre cada letra
+        print(letter, end=' ')
+    print()
+
+def getGuess(alreadyGuessed):
+    # Devuelve la letra que ingresó el jugador. Esta función asegura que el jugador ingrese una sola letra y no otra.
+    while True:
+        print('Guess a letter.')
+        guess = input()
+        guess = guess.lower()
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('You have already guessed that letter. Choose again.')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter a LETTER.')
+        else:
+            return guess
+
+def playAgain():
+    # Esta función devuelve True si el jugador quiere volver a jugar; de lo contrario, devuelve False.
+    print('Do you want to play again? (yes or no)')
+    return input().lower().startswith('y')
+
+
+print('H A N G M A N')
+
+difficulty = 'X'
+while difficulty not in 'EMH':
+  print('Enter difficulty: E - Easy, M - Medium, H - Hard')
+  difficulty = input().upper()
+if difficulty == 'M':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+if difficulty == 'H':
+    del HANGMAN_PICS[8]
+    del HANGMAN_PICS[7]
+    del HANGMAN_PICS[5]
+    del HANGMAN_PICS[3]
+
+missedLetters = ''
+correctLetters = ''
+secretWord, secretSet = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    print('The secret word is in the set: ' + secretSet)
+    displayBoard(missedLetters, correctLetters, secretWord)
+
+    # Deja que el jugador escriba una letra.
+    guess = getGuess(missedLetters + correctLetters)
+
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+
+        # Compruebe si el jugador ha ganado
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+            print('Yes! The secret word is "' + secretWord + '"! You have won!')
+            gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+
+        # Comprueba si el jugador ha adivinado demasiadas veces y ha perdido.
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            displayBoard(missedLetters, correctLetters, secretWord)
+            print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' + str(len(correctLetters)) + ' correct guesses, the word was "' + secretWord + '"')
+            gameIsDone = True
+
+    # Pregúntale al jugador si quiere volver a jugar (pero solo si el juego está terminado).
+    if gameIsDone:
+        if playAgain():
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord, secretSet = getRandomWord(words)
+        else:
+            break
